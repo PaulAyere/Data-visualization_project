7333f6e96396302a3a0f34a2e5a6b07128f4002a
Paulinus Ayere Aghughu <paulynpen@gmail.com>
2023-06-28 17:58:07 +0200

    Updates

---
+++ canva-data/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Curves from './Components/Curves';
+
+function App() {
+  return (
+    <div className="App">
+      <Curves/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ canva-data/src/Components/Curves.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import CanvasJSReact from '@canvasjs/react-charts';
+import customerData from '../Assets/customerData';
+
+// const CanvasJS = CanvasJSReact.CanvasJS;
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+const Curves = () => {
+  const [selectedCurve, setSelectedCurve] = useState(customerData[0].id);
+
+  const handleCurveChange = (event) => {
+    setSelectedCurve(event.target.value);
+  };
+
+  const options = {
+    // Options configuration
+    data: customerData.map(({ id, dataPoints }) => ({
+      type: 'spline',
+      name: id,
+      showInLegend: true,
+      dataPoints: dataPoints,
+      visible: id === selectedCurve,
+    })),
+  };
+
+  return (
+    <div>
+      <h1>Number of New Customers</h1>
+      <div>
+        <label>
+          Select Curve:
+          <select value={selectedCurve} onChange={handleCurveChange}>
+            {customerData.map(({ id }) => (
+              <option key={id} value={id}>
+                {id}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
+      <CanvasJSChart options={options} />
+    </div>
+  );
+};
+
+export default Curves;
